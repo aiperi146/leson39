86736779d5215fbf7b55a3e79825e4fe947c6d87
Aiperi Anarbekovna <ajperikamcybekova146@gmail.com>
2024-02-29 13:01:59 +0600

    h1

--- main.js
+++ main.js
@@ -12,5 +12,23 @@ function validateForm() {
     }
 
 
-    return true; 
+    return true;
 }
+// 4.Для описания требований к полям желательно использовать Map, где ключом является сам элемент поля, либо объект, где ключом является его имя
+  var fieldRequirements = new Map();
+  fieldRequirements.set(document.getElementById('fullName'), 'Поле ФИО обязательно для заполнения.');
+  fieldRequirements.set(document.getElementById('contact'), 'Поле Контакт обязательно для заполнения.');
+  fieldRequirements.set(document.getElementById('password'), 'Поле Пароль обязательно для заполнения.');
+
+  function validateForm() {
+      var isValid = true;
+
+      fieldRequirements.forEach(function (requirement, field) {
+          if (!field.value) {
+              alert(requirement);
+              isValid = false;
+          }
+      });
+
+      return isValid;
+  }
